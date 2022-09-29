2c3afce51858d2a5f7d85b7f1df21cb557f7ac25
Tiberiu Simion Voicu <tiberiu.voicu@moodys.com>
2022-09-29 11:00:28 -0400

    Working chat

--- src/globalChat.tsx
+++ src/globalChat.tsx
@@ -1,9 +1,57 @@
-import { createSupabaseClient } from 'useSupabase'
+import { useState, useRef, useEffect } from 'react'
+import { createSupabaseClient, useMessages } from 'useSupabase'
+import { Input, Box, Text, Button } from '@chakra-ui/react'
 
 const client = createSupabaseClient()
-const userName = 'giorgio_pea'
+const userName = 'Tibi'
 const defaultChannelId = 'd27fe1d8-a6c9-4d7f-856a-d5a30f7c613a'
 
 export const GlobalChatBox = () => {
-    return <h1 style={{ margin: 'auto' }}>Hello World</h1>
+    const [inputValue, updateInputValue] = useState('')
+    const ref = useRef(null)
+    const [messages, createMessages] = useMessages(
+        client,
+        defaultChannelId,
+        userName,
+    )
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.scrollIntoView({ behaviour: 'smooth' })
+        }
+    }, [messages])
+    return (
+        <Box margin="auto">
+            <Box
+                height="700px"
+                width="600px"
+                border="1px solid lightgrey"
+                borderRadius="5px"
+                marginBottom="10px"
+                padding="10px"
+                overflow="auto"
+            >
+                {messages.map((m) => (
+                    <Box
+                        key={m.id}
+                        display="flex"
+                        justifyContent="space-between"
+                    >
+                        <Text>{m.text}</Text>
+                        <Text>{m.user_name}</Text>
+                    </Box>
+                ))}
+                <Box display="hidden" ref={ref} />
+            </Box>
+            <Box display="flex" flexDirection="row">
+                <Input
+                    marginRight="5px"
+                    onChange={(e) => updateInputValue(e.target.value)}
+                    value={inputValue}
+                />
+                <Button onClick={() => createMessages(inputValue)}>
+                    Send message
+                </Button>
+            </Box>
+        </Box>
+    )
 }
